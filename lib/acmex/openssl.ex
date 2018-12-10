012b04d0829a5a9cb018a97d9b2f5394048e54b6
Sérgio Rodrigues <sergioaugrod@gmail.com>
2018-12-10 21:29:24 -0200

    Add doc for openssl

--- lib/acmex/openssl.ex
+++ lib/acmex/openssl.ex
@@ -1,7 +1,11 @@
 defmodule Acmex.OpenSSL do
-  @moduledoc false
+  @moduledoc """
+  This module provides functions to generate a Private Key and Certificate Signing Request.
+  """
 
-  @default_rsa_size 2048
+  @type rsa_key_sizes :: 2048 | 3082 | 4096
+
+  @rsa_key_sizes [2048, 3072, 4096]
   @subject_keys %{
     common_name: "CN",
     country_name: "C",
@@ -11,13 +15,45 @@ defmodule Acmex.OpenSSL do
     state_or_province: "ST"
   }
 
-  def generate_key(:rsa, key_path) do
-    case openssl(~w(genrsa -out #{key_path} #{@default_rsa_size})) do
+  @doc """
+  Generates a RSA private key.
+
+  ## Parameters
+
+    - type: Private key type.
+    - key_path: Private key path.
+    - size: Private key size.
+
+  ## Examples
+
+      iex> Acmex.OpenSSL.generate_key(:rsa, "/tmp/private.key")
+      {:ok, "/tmp/private.key"}
+
+  """
+  @spec generate_key(:rsa, binary(), rsa_key_sizes()) :: {:ok, binary()} | {:error, binary()}
+  def generate_key(:rsa, key_path, size \\ 2048) when size in @rsa_key_sizes do
+    case openssl(~w(genrsa -out #{key_path} #{size})) do
       {:ok, _} -> {:ok, key_path}
       {:error, error} -> {:error, error}
     end
   end
 
+  @doc """
+  Generates a Certificate Signing Request.
+
+  ## Parameters
+
+    - key_path: Private key path.
+    - subject: Subject attributes.
+
+  ## Examples
+
+      iex> subject = %{common_name: "example.com"}
+      iex> Acmex.OpenSSL.generate_csr("/tmp/private.key", subject)
+      {:ok, <<48, 130, 2, 91, 48, 1, ...>>}
+
+  """
+  @spec generate_csr(binary(), Map.t()) :: {:ok, bitstring()} | {:error, binary()}
   def generate_csr(key_path, subject) do
     openssl(~w(req -new -nodes -key #{key_path} -subj #{format_subject(subject)} -outform DER))
   end
